1d06ff137b688d8c1139026bf83deff85172612c
MatthewMorisky <matthew.morisky@gmail.com>
2020-10-16 16:05:33 -0600

    Created the New ArrowPowerUI Branch

--- Assets/Scripts/PlayerShooting.cs
+++ Assets/Scripts/PlayerShooting.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerShooting : MonoBehaviour
 {
     
-    public GameObject arrowPrefab;
+    public GameObject ArrowPrefab;
+    public GameObject ShotPowerFill;
     
     public float minForce = 0f;
     public float maxForce = 50f; 
@@ -28,20 +30,21 @@ public class PlayerShooting : MonoBehaviour
         {
             timer += Time.deltaTime;
             timer = Mathf.Clamp(timer, 0, timeToGetToMaxForce);
-            shootingStrength = Mathf.Lerp(minForce, maxForce, timer / timeToGetToMaxForce);
         }
         else if (Input.GetMouseButtonUp(0))
         {
-            GameObject arrowObject = Instantiate(arrowPrefab) as GameObject;
+            GameObject arrowObject = Instantiate(ArrowPrefab) as GameObject;
             arrowObject.transform.position = transform.position + transform.forward;
             arrowObject.transform.forward = transform.forward;
             arrowObject.GetComponent<Rigidbody>().AddForce(transform.forward * shootingStrength, ForceMode.Impulse);
 
-            timer = minForce;
+            timer = 0;
             shootingStrength = minForce;
         }
 
+        shootingStrength = Mathf.Lerp(minForce, maxForce, timer / timeToGetToMaxForce);
+        ShotPowerFill.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, 300 * (timer / timeToGetToMaxForce));
         //Debug.Log(shootingStrength);
-       
+
     }
 }
